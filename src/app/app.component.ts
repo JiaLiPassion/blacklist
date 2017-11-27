d7bafffbc3725e66f99224573d1f3e6b62c7b495
JiaLi.Passion <JiaLi.Passion@gmail.com>
2017-11-28 00:16:52 +0900

    add sample for blacklist events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+
+declare let Zone: any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  click1() {
+    document.onscroll = function() {
+      console.log('on property in Zone', Zone.current.name);
+    };
+  }
+
+  click2() {
+    document.addEventListener('scroll', function() {
+      console.log('addEventListener in Zone', Zone.current.name);
+    });
+  }
+
+  @HostListener('document:scroll')
+  click3() {
+    console.log('HostListener in Zone', Zone.current.name);
+  }
 }
